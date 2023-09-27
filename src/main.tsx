d228b21c5ba79273556c6cc4ed71c0958a279cd5
antoinefilleul <antoine.filleul@epitech.eu>
2023-09-27 09:58:38 +0200

    [add] continuing headers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import MenuListComposition from './layout/button.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MenuListComposition />
+  </React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import path from 'path';
+import progress from 'vite-plugin-progress';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), splitVendorChunkPlugin(), { ...basicSsl(), apply: 'serve' }, progress()],
+  server: {
+    https: true,
+    port: 3000
+  },
+  optimizeDeps: {},
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      './runtimeConfig': './runtimeConfig.browser'
+    }
+  }
+});
