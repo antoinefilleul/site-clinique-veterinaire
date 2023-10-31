de8e9cddd33619d2c01ae3d6d21d04f2625d8807
antoinefilleul <antoine.filleul@epitech.eu>
2023-10-31 16:14:14 +0100

    new style for e boutique and urgence

--- src/pages/e-boutique.tsx
+++ src/pages/e-boutique.tsx
@@ -1,10 +1,18 @@
+import MenuListComposition from "../layout/button"
+import styles from "./boutique.module.scss"
 
 export default function Eboutique() {
     return (
-        <div>
+        <div className={styles.boutique}>
+            <header>
+            <MenuListComposition/>
+            <img src="./src/assets/logo.jpg" alt="logo" />
             <h1>E-BOUTIQUE</h1>
-            <h2>Nous travaillons avec Chronovet. C’est un site de vente en ligne de produits vétérinaires en vente sans ordonnance. Vous y trouverez de l’alimentation, des produits d’hygiène... aux prix d’internet. Vous précisez que vous voulez être livré à notre clinique et en 24 à 48 h, vos produits sont chez nous, vous n’avez plus qu’à passer les chercher.</h2>
-            <a></a>
+            <div className={styles.content}>
+            <h2>Nous travaillons avec Chronovet. C’est un site de vente en ligne de produits vétérinaires en vente sans ordonnance. Vous y trouverez de l’alimentation, des produits d’hygiène... aux prix d’internet. Vous précisez que vous voulez être livré à notre clinique et en 24 à 48h, vos produits sont chez nous, vous n’avez plus qu’à passer les chercher.</h2>
+            <a className={styles.lien} href="https://www.chronovet.fr/connexion?create_account=1&d_clinic=1650?create_account=1&d_clinic=1650">boutique en ligne</a>
+            </div>
+            </header>
         </div>
     )
 }
