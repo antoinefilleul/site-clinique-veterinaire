a1f6939753f978e015f29be01d138dd072911421
antoinefilleul <antoine.filleul@epitech.eu>
2023-11-09 11:32:30 +0100

    better link button and header style todo:link to paraph and cgf style

--- src/pages/accueil.tsx
+++ src/pages/accueil.tsx
@@ -23,8 +23,10 @@ export default function Accueil() {
     )
   return (
     <div className={styles.accueil}>
+      <div className={styles.menustick}>
+        <MenuListComposition/>
+      </div>
       <div className={styles.header} style={{ backgroundImage: `url(${bgUrl})` }}>
-      <MenuListComposition/>
       <img src={logoUrl} alt="logo" />
       <h1>Bienvenue à la clinique vétérinaire VET SUR JALLES</h1>
       <h1>DR Dominique VENEQUE</h1>
@@ -34,8 +36,8 @@ export default function Accueil() {
         <h2 id = "equipe">Equipe</h2>
         <h3>DR Dominique VENEQUE</h3>
         <h4>diplomée de l'Ecole Nationale Vétérinaire de Maisons Alfort en 1991.</h4>
-        <div className={styles.triangle}></div>
       </div>
+      <div className={styles.triangle}></div>
       <div className={styles.content}>
         <h2>Nous Joindre</h2>
         <h3>adresse : 1521 Avenue de Bordeaux 3317 ST Jean d'Illac</h3>
@@ -48,29 +50,35 @@ export default function Accueil() {
             {list_tarif}
           </div>
         </div>
-        <div className={styles.CGF}>
+        <div className={styles.CGF1}>
         <h2 id = "CGF">Conditions Générales de Fonctionnement</h2>
-        <h3>Les présentes conditions sont consultables à l’accueil de notre clinique ainsi que sur le site internet de la clinique. Tout acte effectué sur un patient dans notre établissement est soumis aux conditions générales de fonctionnement que le propriétaire ou détenteur de l’animal déclare accepter sans conditions.</h3>
-        <h3>Notre établissement de soins est classé « clinique vétérinaire pour animaux de compagnie » conformément à l’arrêté du 15 mars 2015 relatifs aux établissements de soins vétérinaires et du cahier des charges publié sur le site internet de l’ordre des vétérinaires www.veterinaire.fr. La clinique est située au 1521 avenue de Bordeaux à saint Jean d’Illac, vous pouvez nous joindre par téléphone au 0533090800 ou par mail à l’adresse vetsurjalles@gmail.com</h3>
-        <h3>La clinique est ouverte au public du lundi au vendredi de 8h30 à 12h30 et de 14h à 18h30. Les consultations ont lieu uniquement sur rendez-vous sauf cas d’urgence. Pour la sécurité de tous, les chiens doivent être tenus en laisse et les chats doivent être dans des cages de transport. Un docteur vétérinaire est présent dans la clinique pendant les heures d’ouverture, sauf cas de force majeure.</h3>
-        <h3>Nous assurons les consultations généralistes, la vente de médicaments ( conformément à la législation sur la pharmacie vétérinaire en vigueur), de produits d’hygiène, d’aliments physiologiques ou diététiques pour les chiens et les chats, nous pratiquons aussi :</h3>
-        <h4>- chirurgies de convenance</h4>
-        <h4>- chirurgie générale</h4>
-        <h4>- chirurgie orthopédique et traumatologique</h4>
-        <h4>- analyses sanguines : hématologie ( Procyte One) biochimie ( Catalyst One)</h4>
-        <h4>- radiologie numérique : les radios vous sont envoyées par mail</h4>
-        <h4>- échographie</h4>
-        <h4>- réanimation, hospitalisation</h4>
-        <h4>- laser</h4>
-        <h4>- anesthésie générale</h4>
-        <h3>Les animaux hospitalisés font l’objet de soins attentifs et réguliers. Vous pouvez rendre visite à votre compagnon en accord avec la charte d’hospitalisation que nous vous ferons signer. En dehors des heures d’ouverture, un vétérinaire se déplace pour assurer les soins, autant que de besoin et au rythme où il le juge nécessaire. En cas de nécessité médicale, si l’animal a besoin de soins de réanimation ou d’une surveillance continue et permanente, un transfert de celui-ci sera organisé vers un établissement de soins vétérinaires ouvert 24h/24. Ce transfert sera assuré par le propriétaire de l’animal en respectant les consignes données par le vétérinaire.</h3>
-        <h3>En cas d’urgence en dehors des horaires d’ouverture, la permanence et continuité de soins est assurée selon les conditions suivantes :</h3>
-        <h4>- entre 12h30 et 14h, les jours d’ouverture, le vétérinaire assure la permanence téléphonique et recevra les urgences en fonction du degré d’urgence.</h4>
-        <h4>- la nuit et les jours de fermeture, l’établissement assurant la continuité des soins est la clinique Aquivet à Eysines, 19 avenue de la forêt.</h4>
-        <h3>Tout acte sur un animal ( traitement médicamenteux, anesthésie, chirurgie) comporte des risques dont nous informons le propriétaire. Cette information se fera verbalement dans le cadre de la pratique courante ou par écrit ( formulaire de consentement éclairé, charte d’hospitalisation) lors de traitements lourds, de chirurgie et d’hospitalisation.</h3>
-        <h3>Les tarifs des principaux actes sont affichés à l’accueil, ils sont aussi disponibles sur le site internet de la clinique. Pour tout acte pratiqué, le montant vous sera indiqué par oral lors d’une consultation ou par écrit lors d’une chirurgie ou d’une hospitalisation. La nature aléatoire de certains actes, maladies, chirurgies peut rendre difficile l’établissement d’un devis précis. Tout changement ne sera fait qu’avec votre accord. Tout devis signé engage le propriétaire à le régler à la restitution de son animal. Tous nos actes sont soumis à une TVA de 20 %. Une facture ( papier ou informatique) sera établie à l’issue de toute transaction. Les honoraires sont payables comptant à lors de la restitution de l’animal, ou en fin de consultation en carte bancaire ou en espèces.</h3>
-        <h3>En cas de litige à caractère déontologique, le propriétaire peut s’adresser au Conseil Régional de l’Ordre des Vétérinaires de la région Nouvelle Aquitaine, 40 rue de Belfort, 33000 Bordeaux. La clinique est assurée pour sa responsabilité professionnelle par AXA, agence Tartary, 500 avenue du Las, 33127 Saint jean d’Illac.</h3>
-        <h3>Conformément à la loi « Informatique et liberté » du 6 janvier 1978, le client dispose d’un droit d’accès, de modification, de suppression des données le concernant auprès du docteure Vénèque. Tout vétérinaire est soumis au secret professionnel, aucune information recueillie lors de la consultation ne pourra être divulguée sauf commission rogatoire selon les conditions prévues par la loi.</h3>
+        <ul>
+        <li>Les présentes conditions sont consultables à l’accueil de notre clinique ainsi que sur le site internet de la clinique. Tout acte effectué sur un patient dans notre établissement est soumis aux conditions générales de fonctionnement que le propriétaire ou détenteur de l’animal déclare accepter sans conditions.</li>
+        <li>Notre établissement de soins est classé « clinique vétérinaire pour animaux de compagnie » conformément à l’arrêté du 15 mars 2015 relatifs aux établissements de soins vétérinaires et du cahier des charges publié sur le site internet de l’ordre des vétérinaires www.veterinaire.fr. La clinique est située au 1521 avenue de Bordeaux à saint Jean d’Illac, vous pouvez nous joindre par téléphone au 0533090800 ou par mail à l’adresse vetsurjalles@gmail.com</li>
+        <li>La clinique est ouverte au public du lundi au vendredi de 8h30 à 12h30 et de 14h à 18h30. Les consultations ont lieu uniquement sur rendez-vous sauf cas d’urgence. Pour la sécurité de tous, les chiens doivent être tenus en laisse et les chats doivent être dans des cages de transport. Un docteur vétérinaire est présent dans la clinique pendant les heures d’ouverture, sauf cas de force majeure.</li>
+        <li>Nous assurons les consultations généralistes, la vente de médicaments ( conformément à la législation sur la pharmacie vétérinaire en vigueur), de produits d’hygiène, d’aliments physiologiques ou diététiques pour les chiens et les chats, nous pratiquons aussi :<br></br>
+        - chirurgies de convenance<br></br>
+        - chirurgie générale<br></br>
+        - chirurgie orthopédique et traumatologique<br></br>
+        - analyses sanguines : hématologie ( Procyte One) biochimie ( Catalyst One)<br></br>
+        - radiologie numérique : les radios vous sont envoyées par mail<br></br>
+        - échographie<br></br>
+        - réanimation,hospitalisation<br></br>
+        - laser<br></br>
+        - anesthésie générale</li>
+        </ul>
+        </div>
+        <div className={styles.CGF2}>
+        <ul>
+        <li>Les animaux hospitalisés font l’objet de soins attentifs et réguliers. Vous pouvez rendre visite à votre compagnon en accord avec la charte d’hospitalisation que nous vous ferons signer. En dehors des heures d’ouverture, un vétérinaire se déplace pour assurer les soins, autant que de besoin et au rythme où il le juge nécessaire. En cas de nécessité médicale, si l’animal a besoin de soins de réanimation ou d’une surveillance continue et permanente, un transfert de celui-ci sera organisé vers un établissement de soins vétérinaires ouvert 24h/24. Ce transfert sera assuré par le propriétaire de l’animal en respectant les consignes données par le vétérinaire.</li>
+        <li>En cas d’urgence en dehors des horaires d’ouverture, la permanence et continuité de soins est assurée selon les conditions suivantes :<br></br>
+        - entre 12h30 et 14h, les jours d’ouverture, le vétérinaire assure la permanence téléphonique et recevra les urgences en fonction du degré d’urgence.<br></br>
+        - la nuit et les jours de fermeture, l’établissement assurant la continuité des soins est la clinique Aquivet à Eysines, 19 avenue de la forêt.</li>
+        <li>Tout acte sur un animal ( traitement médicamenteux, anesthésie, chirurgie) comporte des risques dont nous informons le propriétaire. Cette information se fera verbalement dans le cadre de la pratique courante ou par écrit ( formulaire de consentement éclairé, charte d’hospitalisation) lors de traitements lourds, de chirurgie et d’hospitalisation.</li>
+        <li>Les tarifs des principaux actes sont affichés à l’accueil, ils sont aussi disponibles sur le site internet de la clinique. Pour tout acte pratiqué, le montant vous sera indiqué par oral lors d’une consultation ou par écrit lors d’une chirurgie ou d’une hospitalisation. La nature aléatoire de certains actes, maladies, chirurgies peut rendre difficile l’établissement d’un devis précis. Tout changement ne sera fait qu’avec votre accord. Tout devis signé engage le propriétaire à le régler à la restitution de son animal. Tous nos actes sont soumis à une TVA de 20 %. Une facture ( papier ou informatique) sera établie à l’issue de toute transaction. Les honoraires sont payables comptant à lors de la restitution de l’animal, ou en fin de consultation en carte bancaire ou en espèces.</li>
+        <li>En cas de litige à caractère déontologique, le propriétaire peut s’adresser au Conseil Régional de l’Ordre des Vétérinaires de la région Nouvelle Aquitaine, 40 rue de Belfort, 33000 Bordeaux (tel: 05 56 24 56 93, adresse électronique : cro.nouvelle-aquitaine@ordre.veterinaire/fr). La clinique est assurée pour sa responsabilité professionnelle par AXA, agence Tartary, 500 avenue du Las, 33127 Saint jean d’Illac.</li>
+        <li>Conformément à la loi « Informatique et liberté » du 6 janvier 1978, le client dispose d’un droit d’accès, de modification, de suppression des données le concernant auprès du docteure vétérinaire Vénèque. Tout vétérinaire est soumis au secret professionnel, aucune information recueillie lors de la consultation ne pourra être divulguée sauf commission rogatoire selon les conditions prévues par la loi.</li>
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/urgence.tsx
+++ src/pages/urgence.tsx
@@ -1,14 +1,15 @@
 import MenuListComposition from "../layout/button"
 import styles from "./urgence.module.scss"
 import bgUrl from '../assets/image.jpg'
+import logoUrl from '../assets/logo.jpg'
 
 export default function Urgence() {
     return (
         <div className={styles.urgence}>
             <div className={styles.header} style={{ backgroundImage: `url(${bgUrl})` }}>
                 <MenuListComposition/>
-                <img src="./src/assets/logo.jpg" alt="logo" />
-            <h1>URGENCES</h1>
+                <img src={logoUrl} alt="logo" />
+.            <h1>URGENCES</h1>
             <div className={styles.content}>
             <h2>En cas d’urgence pendant les heures d’ouverture, appelez-nous avant de venir : d’une part, nous pourrons évaluer avec vous par téléphone le degré d’urgence et apaiser vos craintes, nous pourrons aussi vous donner des conseils ( par exemple, en cas de contact avec des chenilles processionnaires, bien rincer l’animal avant de l’amener) et d’autre part, cela nous permettra de préparer ce qu’il faut pour vous recevoir le plus efficacement possible. En dehors des heures d’ouverture, nous avons signé une permanence de continuité de soins avec le Centre Hospitalier Vétérinaire Aquivet , 19 allée de la forêt à Eysines. Leur numéro de téléphone est le 05 56 95 01 01.</h2>
             </div>
